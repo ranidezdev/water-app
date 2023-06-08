69bdc1285cb2a6c25f279453722627ff2d1915ed
ranidezdev <pz86qeny@studserv.uni-leipzig.de>
2023-06-08 20:51:33 +0200

    Added StorageManager.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet  } from 'react-native';
 import Overview from './Overview';
 import CalenderView from './CalenderView';
 import NavigationBar from '../components/NavigationBar';
+import StorageManager from '../utils/StorageManager';
 
 export default function HomeScreen() {
   const [currentView, setCurrentView] = useState('overview');
+
+  useEffect(() => {
+    StorageManager.checkAndCreateEntries();
+
+    // console.log("FluidAmount for 06/08/23: ");
+    // StorageManager.getFluidAmount("06/08/23").then(fluidAmount => {
+    //   console.log(fluidAmount); 
+    // });
+  }, []);
 
   const handleOverviewPress = () => {
     setCurrentView('overview');
@@ -27,8 +37,7 @@ export default function HomeScreen() {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+      flex: 1,
+    },
 });
-
